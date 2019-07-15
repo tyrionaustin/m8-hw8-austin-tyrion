493b8b020e5b9df11fd940d2d9cc7908828100c2
Tyrion Austin <tyrion.austin@ufl.edu>
2019-07-14 19:38:36 -0500

    Add for loop to javascript file

--- hw8-starter-code/main.js
+++ hw8-starter-code/main.js
@@ -1,7 +1,18 @@
 // Global Variables
-
+const myBoxes = document.getElementsByClassName('block');
 
 // The Functionality
+function loopThrough() {
 
+
+
+  for(let i = 0; i < this.textConent.length; i++) {
+
+  };
+
+};
 
 // The Event Listener
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
